77cd3973f8e74f4ba4cacece486e98f6277a723b
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2011-04-14 17:53:56 +0900

    11/04/14 update

--- .emacs.d/site-start.d/30_redo+.el
+++
@@ -1,8 +0,0 @@
-;; INSTALL
-;; (install-elisp-from-emacswiki "redo+.el")
-
-(require 'redo+)
-(global-set-key (kbd "C-M-_") 'redo)
-(setq undo-no-redo t)
-(setq undo-limit 600000)
-(setq undo-strong-limit 900000)
