add397149807f105d4e0b4427257bab6b9aced1c
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2011-05-11 19:55:21 +0900

    remove backup files code added

--- .emacs.d/etc/copy-dot-emacs-dir-to-dot-emacs.sh
+++ .emacs.d/etc/copy-dot-emacs-dir-to-dot-emacs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 cd ~/.emacs.d/
-cp -r ~/.emacs.d/site-start.d/ ~/dot-emacs/.emacs.d/
-cp -r ~/.emacs.d/elisp/ ~/dot-emacs/.emacs.d/
+cp -r ~/.emacs.d/site-start.d/   ~/dot-emacs/.emacs.d/
+cp -r ~/.emacs.d/elisp/          ~/dot-emacs/.emacs.d/
 cp -r ~/.emacs.d/etc/mysnippets/ ~/dot-emacs/.emacs.d/etc/
-cp -r ~/.emacs.d/plugins/ ~/dot-emacs/.emacs.d/
+cp -r ~/.emacs.d/plugins/        ~/dot-emacs/.emacs.d/
+cd ~/dot-emacs/.emacs.d/
+rm -f site-start.d/*.el~
+rm -f site-start.d/\#*
+rm -f site-start.d/.#*
 echo "End copy .emacs.d"
