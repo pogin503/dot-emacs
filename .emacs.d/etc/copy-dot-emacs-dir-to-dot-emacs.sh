6b2b21bd4ff39023ccfd81236404433de31b5b85
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2011-05-18 02:33:48 +0900

    modified

--- .emacs.d/etc/copy-dot-emacs-dir-to-dot-emacs.sh
+++ .emacs.d/etc/copy-dot-emacs-dir-to-dot-emacs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd ~/.emacs.d/
-cp -r ~/.emacs.d/site-start.d/   ~/dot-emacs/.emacs.d/
-cp -r ~/.emacs.d/elisp/          ~/dot-emacs/.emacs.d/
-cp -r ~/.emacs.d/etc/mysnippets/ ~/dot-emacs/.emacs.d/etc/
-cp -r ~/.emacs.d/plugins/        ~/dot-emacs/.emacs.d/
-cd ~/dot-emacs/
-rm -rf .emacs.d/site-start.d/*.el~
+cp -r site-start.d/   ~/dot-emacs/.emacs.d/
+cp -r elisp/          ~/dot-emacs/.emacs.d/
+cp -r etc/mysnippets/ ~/dot-emacs/.emacs.d/etc/
+cp -r plugins/        ~/dot-emacs/.emacs.d/
+cp etc/*.sh
 cd ~/dot-emacs/.emacs.d/
+rm -rf site-start.d/*.el~
 rm -rf site-start.d/\#*
 rm -rf site-start.d/.#*
 echo "End copy .emacs.d"
--- .emacs.d/etc/copy-dot-emacs-to-dot-emacs-dir.sh
+++ .emacs.d/etc/copy-dot-emacs-to-dot-emacs-dir.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 cd ~/dot-emacs/.emacs.d/
-cp -r site-start.d/ ~/.emacs.d/
-cp -r elisp/ ~/.emacs.d/
-cp -r etc/mysnippets/ ~/.emacs.d/etc/
-cp -r plugins/ ~/.emacs.d/
+git pull
+cp -r site-start.d/      ~/.emacs.d/
+cp -r elisp/             ~/.emacs.d/
+cp -r etc/mysnippets/    ~/.emacs.d/etc/
+cp -r plugins/           ~/.emacs.d/
+cp etc/*.sh
 cd ~/.emacs.d/
 rm -rf site-start.d/*.el~
 rm -rf site-start.d/\#*
