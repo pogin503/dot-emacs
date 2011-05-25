7fcba4764e6e611a712eae25da937031a3adf2a3
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2011-05-25 17:41:26 +0900

    my-smartchr-lisp-keybindings were added

--- .emacs.d/site-start.d/30_init-smartchr.el
+++ .emacs.d/site-start.d/30_init-smartchr.el
@@ -22,14 +22,23 @@
 (defun smartchr-custom-keybindings ()
   (local-set-key (kbd "=") (smartchr '(" = " " == "  "=")))
   ;; !! がカーソルの位置
+  (local-set-key (kbd "+") (smartchr '("+" " + " "++" " += ")))
+  (local-set-key (kbd "-") (smartchr '("-" " - " "--" " -= ")))
   (local-set-key (kbd "(") (smartchr '("(`!!')" "(")))
   (local-set-key (kbd "[") (smartchr '("[`!!']" "[ [`!!'] ]" "[")))
   (local-set-key (kbd "{") (smartchr '("{\n`!!'\n}" "{`!!'}" "{")))
   (local-set-key (kbd "`") (smartchr '("\``!!''" "\`")))
   (local-set-key (kbd "\"") (smartchr '("\"`!!'\"" "\"")))
   (local-set-key (kbd ">") (smartchr '(">" " => " " => '`!!''" " => \"`!!'\"")))
+  (local-set-key (kbd "<") (smartchr '("<" "<`!!'>")))
   )
 
+(defun my-smartchr-lisp-keybindings ()
+  (local-set-key (kbd "(") (smartchr '("(`!!')" "(")))
+  (local-set-key (kbd "[") (smartchr '("[`!!']" "[ [`!!'] ]" "[")))
+  (local-set-key (kbd "`") (smartchr '("\``!!''" "\`")))
+  (local-set-key (kbd "\"") (smartchr '("\"`!!'\"" "\"")))
+  )  
 
 (defun smartchr-custom-keybindings-objc ()
   (local-set-key (kbd "@") (smartchr '("@\"`!!'\"" "@"))))
@@ -41,3 +50,8 @@
 (add-hook 'javascript-mode-hook 'my-smartchr-setting)
 (add-hook 'ruby-mode-hook 'my-smartchr-setting)
 (add-hook 'cperl-mode-hook 'my-smartchr-setting)
+(add-hook 'emacs-lisp-mode-hook 'my-smartchr-lisp-keybindings)
+
+
+
+
