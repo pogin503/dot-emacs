5adc669a5af51a53c6afa562c2c1f4c638f725fa
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2011-05-28 17:40:47 +0900

    add php conf,js conf, css conf and mmm-mode conf

---
+++ .emacs.d/site-start.d/50_init-php-mode.el
@@ -0,0 +1,9 @@
+(add-to-list 'load-path "~/.emacs.d/plugins/php-mode/")
+(autoload 'php-mode "php-mode")
+(setq auto-mode-alist
+      (cons '("\\.php\\'" . php-mode) auto-mode-alist))
+(setq php-mode-force-pear t)
+(add-hook 'php-mode-user-hook
+  '(lambda ()
+     ;;(setq php-manual-path "/usr/local/share/php/doc/html")
+     (setq php-manual-url "http://www.phppro.jp/phpmanual/")))
