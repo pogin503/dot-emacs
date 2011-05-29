9647ec182f9181057e57f120e43d4ce4babec32d
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2011-05-30 02:53:03 +0900

    add php-mode conf etc

--- .emacs.d/site-start.d/50_init-php-mode.el
+++ .emacs.d/site-start.d/50_init-php-mode.el
@@ -9,3 +9,18 @@
      ;;(setq php-manual-path "/usr/local/share/php/doc/html")
      (setq php-manual-url "http://www.phppro.jp/phpmanual/")))
 
+(add-hook 'php-mode-hook
+          (lambda ()
+            (require 'php-completion)
+            (php-completion-mode t)
+            (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)))
+
+(add-hook  'php-mode-hook
+           (lambda ()
+             (when (require 'auto-complete nil t)
+               (make-variable-buffer-local 'ac-sources)
+               (add-to-list 'ac-sources 'ac-source-php-completion)
+               ;; if you like patial match,
+               ;; use `ac-source-php-completion-patial' instead of `ac-source-php-completion'.
+               ;; (add-to-list 'ac-sources 'ac-source-php-completion-patial)
+               (auto-complete-mode t))))
