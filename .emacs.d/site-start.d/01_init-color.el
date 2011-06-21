ebddd718235d74755bffb0a712ed07f7be4665bc
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2011-06-22 03:33:02 +0900

    Added: php-completion.el Chaenge: color conf, auto-complete conf and php-mode conf
    New      .emacs.d/elisp/php-completion.el
    New      .emacs.d/elisp/php-completion.elc
    Modified .emacs.d/plugins/auto-complete/ac-dict/php-mode
    Modified .emacs.d/site-start.d/01_init-color.el
    Modified .emacs.d/site-start.d/01_init-color.elc
    Modified .emacs.d/site-start.d/02_init-auto-complete.el
    Modified .emacs.d/site-start.d/02_init-auto-complete.elc
    Modified .emacs.d/site-start.d/50_init-php-mode.el
    Modified .emacs.d/site-start.d/50_init-php-mode.elc

--- .emacs.d/site-start.d/01_init-color.el
+++ .emacs.d/site-start.d/01_init-color.el
@@ -1,10 +1,10 @@
-;@see 
+;;@see 
 
 ;; 背景の透過
 ;; (add-to-list 'default-frame-alist '(alpha . (85 20)))
-(add-to-list 'default-frame-alist '(alpha . (92 70)))
+(add-to-list 'default-frame-alist '(alpha . (95 85)))
 
-; タブ文字、全角空白、文末の空白の色付け
+;; タブ文字、全角空白、文末の空白の色付け
 ;; @see http://www.emacswiki.org/emacs/WhiteSpace
 ;; @see http://xahlee.org/emacs/whitespace-mode.html
 (setq whitespace-style '(spaces tabs newline space-mark tab-mark newline-mark))
--- .emacs.d/site-start.d/02_init-auto-complete.el
+++ .emacs.d/site-start.d/02_init-auto-complete.el
@@ -1,14 +1,15 @@
 ;;auto-complete******************************
 (add-to-list 'load-path "~/.emacs.d/auto-complete/")
 
-(req auto-complete)
-(global-auto-complete-mode t)
-(req auto-complete-config)
-(ac-config-default)
+;; (req auto-complete)
+;; (global-auto-complete-mode t)
 
 ;;@see http://stackoverflow.com/questions/4281583/i-get-a-error-when-i-try-install-auto-complete-in-emacs
-(eval-after-load 'auto-complete-config
-    '(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/ac-dict"))
+;; (eval-after-load 'auto-complete-config
+;;     '(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/ac-dict"))
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/ac-dict")
+(req auto-complete-config)
+(ac-config-default)
 ;;end
 
 (ac-flyspell-workaround)
