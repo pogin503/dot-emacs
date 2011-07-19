573df2a277feaf4db71e9eeb093537f1b159be6f
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2011-07-20 02:27:07 +0900

    Added: e2wm files, e2wm init files and  shadow.el

---
+++ .emacs.d/site-start.d/30_init-e2wm.el
@@ -0,0 +1,52 @@
+;;;;  30_init-e2wm.el --
+;; -*- Mode: Emacs-Lisp -*-
+
+;; Created: Tue Jul 19 03:56:19 2011
+;; Last-Updated: Time-stamp: <2011-07-19 15:29:18 ogin> 
+;; URL: 
+;; Keywords: 
+;; Compatibility: 
+;; 
+
+;;; Commentary: 
+;; 
+;; 
+;; 
+
+;; Licence
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;; 
+
+;; 
+;;; Code:
+
+(require 'e2wm)
+(global-set-key (kbd "M-+") 'e2wm:start-management)
+
+;; for 1024x768
+(setq e2wm:c-code-recipe
+  '(| (:left-max-size 35)
+      (- (:upper-size-ratio 0.7)
+         (- (:upper-size-ratio 0.6)
+            files imenu)
+         history)
+      (- (:upper-size-ratio 0.7)
+         main sub)))
+
+(req e2wm-config)
+
+(req e2wm-vcs)
+;; 30_init-e2wm.el ends here
