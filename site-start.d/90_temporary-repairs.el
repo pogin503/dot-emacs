d1c92d7c8ea7724171f7e1840268c79070b901bf
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2011-10-09 16:12:27 +0900

    bugfix

--- site-start.d/90_temporary-repairs.el
+++ site-start.d/90_temporary-repairs.el
@@ -1,6 +1,12 @@
 
-(defun temporary-repairs-yas-keybind ()
+;; (defun temporary-repairs-yas-keybind ()
+;;   '(lambda ()
+;; 	 (interactive)
+;; 	 (insert "k")))
+;;(defvar test-map (make-sparse-keymap))
+
+;; (define-key test-map (kbd "k") '(lambda () (insert "k")))
+(define-key emacs-lisp-mode-map (kbd "k") 
   '(lambda ()
 	 (interactive)
-	 (insert "k")))
-(define-key emacs-lisp-mode-map (kbd "k") 'temporary-repairs-yas-keybind)
+	 (insert "k ")))
