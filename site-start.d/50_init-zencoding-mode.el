9d2b247bc3edcc6f9ad4f253781aae420166cd4f
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2011-11-23 02:24:02 +0900

    comment out text-mode-hook

--- site-start.d/50_init-zencoding-mode.el
+++ site-start.d/50_init-zencoding-mode.el
@@ -2,5 +2,5 @@
 (req zencoding-mode)
 (add-hook 'sgml-mode-hook 'zencoding-mode)
 (add-hook 'html-mode-hook 'zencoding-mode)
-(add-hook 'text-mode-hook 'zencoding-mode)
+;; (add-hook 'text-mode-hook 'zencoding-mode)
 (define-key zencoding-mode-keymap "\C-z" 'zencoding-expand-line)
