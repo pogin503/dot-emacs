d28236fd0e217b224a18e94d163bd3a02181d929
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2011-11-27 03:57:16 +0900

    improve smartchr conf

--- site-start.d/30_init-smartchr.el
+++ site-start.d/30_init-smartchr.el
@@ -1,6 +1,34 @@
-;;@see https://github.com/handle/emacs-settings
-;;@see https://bitbucket.org/sakito/dot.emacs.d/src/7a6d89a5b3c6/site-start.d/init_smartchr.el
-;;@see http://d.hatena.ne.jp/tequilasunset/20101119/p1
+;;;; 30_init-smartchr.el ---- smartchr
+;; -*- Mode: Emacs-Lisp -*-
+
+;; Copyright (C) pogin 
+
+;; Author: pogin
+;; Keywords: tools
+
+;;; License
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;; @see https://github.com/handle/emacs-settings
+;; @see https://bitbucket.org/sakito/dot.emacs.d/src/7a6d89a5b3c6/site-start.d/init_smartchr.el
+;; @see http://d.hatena.ne.jp/tequilasunset/20101119/p1
+;;
 ;; INSTALL
 ;; (install-elisp "https://github.com/imakado/emacs-smartchr/raw/master/smartchr.el")
 
@@ -66,8 +94,12 @@
   (local-set-key (kbd "(") (smartchr '("(`!!')" "(")))
   (local-set-key (kbd "[") (smartchr '("[`!!']" "[[`!!']]" "[")))
   (local-set-key (kbd "{") (smartchr '("{" "{`!!'}" my-smartchr-braces)))
+  ;;バッククォート
   (local-set-key (kbd "`") (smartchr '("\``!!''" "\`")))
+  ;;ダブルクォーテーション
   (local-set-key (kbd "\"") (smartchr '("\"`!!'\"" "\"")))
+  ;;シングルクォート
+  (local-set-key (kbd "\'") (smartchr '("\'`!!'\'" "\'")))
   (local-set-key (kbd ">") (smartchr '(">" " > " " => " " >> ")))
   (local-set-key (kbd "<") (smartchr '("<" " < " " << " "<`!!'>")))
   (local-set-key (kbd ",") (smartchr '(", " ",")))
@@ -79,6 +111,7 @@
   (local-set-key (kbd "/") (smartchr '("/" "/* `!!' */" my-smartchr-comment)))
   (local-set-key (kbd ";") (smartchr '(";" my-smartchr-semicolon)))
   )
+
 (defun my-smartchr-keybindings-lisp ()
   (local-set-key (kbd "(") (smartchr '("(`!!')" "(")))
   (local-set-key (kbd "[") (smartchr '("[`!!']" "[[`!!']]" "[")))
@@ -119,24 +152,24 @@
   (local-set-key (kbd "=") (smartchr '("=" " = " " == ")))
   (local-set-key (kbd "(") (smartchr '("(`!!')" "(")))
   (local-set-key (kbd "[") (smartchr '("[`!!']" "[[`!!']]" "[")))
-  (local-set-key (kbd "{") (smartchr '("{" "{`!!'}" "{\n`!!'\n}")))
+  (local-set-key (kbd "{") (smartchr '("{" "{`!!'}" my-smartchr-braces)))
+  ;;ダブルクォート
   (local-set-key (kbd "\"") (smartchr '("\"`!!'\"" "\"")))
+  ;;シングルクォート
+  (local-set-key (kbd "\'") (smartchr '("\'`!!'\'" "\'")))
   (local-set-key (kbd "<") (smartchr '("<`!!'>" "</`!!'>" "<")))
   (local-set-key (kbd ",") (smartchr '(", " ",")))
   (local-set-key (kbd ".") (smartchr '("." " . ")))
   (local-set-key (kbd "?") (smartchr '("?" "? `!!' " "<?`!!'?>")))
-  (local-set-key (kbd "!") (smartchr '("!" " != ")))
-  (local-set-key (kbd "&") (smartchr '("&" " && ")))
-  (local-set-key (kbd "|") (smartchr '("|" " || ")))
+  (local-set-key (kbd "!") (smartchr '("!" "<!-- `!!' -->")))
+  (local-set-key (kbd ";") (smartchr '(";" my-smartchr-semicolon)))
+  (local-set-key (kbd ":") (smartchr '(": " ":")))
+  (local-set-key (kbd "/") (smartchr '("/" "/* `!!' */" my-smartchr-comment)))
   )
+(dolist (hook (list
+               'html-mode-hook 
+               'css-mode-hook
+               ))
+  (add-hook hook 'my-smartchr-keybindings-html))
 
-(add-hook 'html-mode-hook 'my-smartchr-keybindings-html)
-
-
-
-
-
-
-
-
-
+;;; 30_init-smartchr.el ends here
