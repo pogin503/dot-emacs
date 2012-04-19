b05c9c53c6a0fa38bc5222603d1d14eb4568a395
pogin <pogin@(none).(none)>
2012-04-19 23:29:07 +0900

    add vi like keybind

--- site-start.d/01_init-keybind.el
+++ site-start.d/01_init-keybind.el
@@ -35,3 +35,43 @@
 ;; Font size
 (define-key global-map (kbd "C-+") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
+
+;; 'o' 次の行に挿入
+(defun edit-next-line ()
+  (interactive)
+  (end-of-line)
+  (newline-and-indent))
+
+;; 'O' 前の行に挿入
+(defun edit-previous-line ()
+  (interactive)
+  (forward-line -1)
+  (if (not (= (current-line) 1))
+      (end-of-line))
+  (newline-and-indent))
+
+(global-set-key (kbd "M-o") 'edit-next-line)
+;; (global-set-key (kbd "S-RET") 'edit-next-line)
+;; (global-set-key (kbd "S-<RET>") 'edit-next-line)
+(global-set-key (kbd "S-<return>") 'edit-next-line)
+(global-set-key (kbd "M-O") 'edit-previous-line)
+(global-set-key (kbd "C-S-<return>") 'edit-previous-line)
+
+;; 'f' 後方の入力した文字の上に移動
+(defun forward-match-char (n)
+  (interactive "p")
+  (let ((c (read-char)))
+    (dotimes (i n)
+      (forward-char)
+      (skip-chars-forward (format "^%s" (char-to-string c))))))
+
+;; 'F' 前方の入力した文字の上に移動
+(defun backward-match-char (n)
+  (interactive "p")
+  (let ((c (read-char)))
+    (dotimes (i n)
+      (skip-chars-backward (format "^%s" (char-to-string c)))
+      (backward-char))))
+
+(global-set-key (kbd "M-l") 'forward-match-char)
+(global-set-key (kbd "M-L") 'backward-match-char)
