65f24ad5ffea62dbfc147da83b7bbba7b15cce3f
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2012-05-26 22:30:03 +0900

    update: haskell-mode conf and solarized-theme, add: ghc-mod

--- site-start.d/50_init-haskell.el
+++ site-start.d/50_init-haskell.el
@@ -7,6 +7,56 @@
 
 (lazyload (haskell-mode) "haskell-mode"
           (require 'haskell-mode)
+          (require 'anything)
+          (require 'anything-config)
+          (require 'anything-match-plugin)
+
+          (defvar anything-c-source-ghc-mod
+            '((name . "ghc-browse-document")
+              (init . anything-c-source-ghc-mod)
+              (candidates-in-buffer)
+              (candidate-number-limit . 9999999)
+              (action ("Open" . anything-c-source-ghc-mod-action))))
+
+          (defun anything-c-source-ghc-mod ()
+            (unless (executable-find "ghc-mod")
+              (error "ghc-mod を利用できません。ターミナルで which したり、*scratch* で exec-path を確認したりしましょう"))
+            (let ((buffer (anything-candidate-buffer 'global)))
+              (with-current-buffer buffer
+                (call-process "ghc-mod" nil t t "list"))))
+
+          (defun anything-c-source-ghc-mod-action (candidate)
+            (interactive "P")
+            (let* ((pkg (ghc-resolve-package-name candidate)))
+              (anything-aif (and pkg candidate)
+                  (ghc-display-document pkg it nil)
+                (message "No document found"))))
+
+          (defun anything-ghc-browse-document ()
+            (interactive)
+            (anything anything-c-source-ghc-mod))
+
+          ;; https://github.com/m2ym/auto-complete
+          (ac-define-source ghc-mod
+            '((depends ghc)
+              (candidates . (ghc-select-completion-symbol))
+              (symbol . "s")
+              (cache)))
+
+          (defun my-ac-haskell-mode ()
+            (setq ac-sources '(ac-source-words-in-same-mode-buffers
+                               ac-source-dictionary
+                               ac-source-ghc-mod)))
+          (defun my-haskell-ac-init ()
+            (when (member (file-name-extension buffer-file-name) '("hs" "lhs"))
+              (auto-complete-mode t)
+              (setq ac-sources '(ac-source-words-in-same-mode-buffers
+                                 ac-source-dictionary
+                                 ac-source-ghc-mod))))
+          (require 'anything-hasktags)
+
+          ;; M-x anything-ghc-browse-document() に対応するキーの割り当て
+          ;; ghc-mod の設定のあとに書いた方がよいかもしれません
           ;; @see http://d.hatena.ne.jp/mizchi/20120426/1335409088
           ;; (require 'flymake)
           ;; (define-key haskell-mode-map (kbd "M-n") 'flymake-goto-next-error)
@@ -81,5 +131,30 @@
 ;;   )
 
 ;; (add-hook 'haskell-mode-hook 'define-haskell-mode-conf)
-(add-hook 'haskell-mode-hook 'auto-complete-mode)
+(add-to-list 'ac-modes 'haskell-mode)
+;; (add-hook 'haskell-mode-hook 'auto-complete-mode)
 
+
+;; ghc-mod
+;; cabal でインストールしたライブラリのコマンドが格納されている bin ディレクトリへのパスを exec-path に追加する
+(add-to-list 'exec-path (concat (getenv "HOME") "/.cabal/bin"))
+;; ghc-flymake.el などがあるディレクトリ ghc-mod を ~/.emacs.d 以下で管理することにした
+;; (add-to-list 'load-path "~/.emacs.d/plugins/ghc-mod") 
+
+(autoload 'ghc-init "ghc" nil t)
+
+(add-hook 'haskell-mode-hook
+  (lambda () (ghc-init) (flymake-mode 1)))
+
+(add-hook 'haskell-mode-hook
+          (lambda()
+            (define-key haskell-mode-map (kbd "C-M-d") 'anything-ghc-browse-document)))
+
+(add-hook 'haskell-mode-hook 'my-ac-haskell-mode)
+
+
+(add-hook 'find-file-hook 'my-haskell-ac-init)
+
+(add-hook 'haskell-mode-hook
+          (lambda()
+            (define-key haskell-mode-map (kbd "C-c j") 'anything-hasktags-select)))
