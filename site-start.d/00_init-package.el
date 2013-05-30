1a89b7e53ee7f14acfdd30b8d7911ea3337fdf24
pogin503 <pogin503@gmail.com>
2013-05-31 05:20:01 +0900

    Add package config

--- site-start.d/00_init-package.el
+++ site-start.d/00_init-package.el
@@ -15,5 +15,49 @@
 
 ;;インストールしたパッケージにロードパスを通してロードする
 (package-initialize)
+;; install packages by package.el
+(mapc
+ (lambda (package)
+   (or (package-installed-p package)
+       (package-install package)))
+ '(
+   ;; starter-kit
+   auto-complete
+   auto-install
+   clojure-mode
+   coffee-mode
+   color-moccur
+   dash
+   e2wm
+   flymake-easy
+   git-gutter
+   haskell-mode
+   helm
+   helm-anything
+   helm-c-moccur
+   helm-c-yasnippet
+   helm-ls-git
+   ht
+   js2-mode
+   magit
+   markdown-mode
+   mmm-mode
+   multiple-cursors
+   nginx-mode
+   org
+   php-mode
+   popwin
+   powerline
+   pretty-lambdada
+   quickrun
+   rhtml-mode
+   ruby-end
+   s
+   session
+   slime
+   solarized-theme
+   starter-kit-ruby
+   yasnippet
+   ))
 
 (require 'melpa)
