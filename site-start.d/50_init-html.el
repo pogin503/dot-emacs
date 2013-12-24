a929c82ffd7238d50ed087d0f1a326966f3b1709
pogin503 <pogin503@gmail.com>
2013-12-24 12:48:09 +0900

    Fix mismatch parenthesis.

--- site-start.d/50_init-html.el
+++ site-start.d/50_init-html.el
@@ -5,22 +5,51 @@
 (require '00_init-macro)
 
 ;; (add-to-load-path "plugins/zencoding")
-(req zencoding-mode)
-(dolist (hook (list
-			   'nxml-mode-hook
-			   'sgml-mode-hook
-			   'html-mode-hook
-			   ))
-  (add-hook hook 'zencoding-mode))
-;; (add-hook 'text-mode-hook 'zencoding-mode)
-(define-key zencoding-mode-keymap (kbd "C-c C-m") 'zencoding-expand-line)
-(define-key zencoding-preview-keymap (kbd "C-c C-m") 'zencoding-preview-accept)
-(require 'multi-web-mode)
-(setq mweb-default-major-mode 'html-mode)
-(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-				  (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-				  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
-(setq mweb-filename-extensions '("htm" "html" "ctp" "phtml"))
-(multi-web-global-mode 1)
+(req web-mode
+
+;;; emacs 23以下の互換
+     ;; (when (< emacs-major-version 24)
+     ;;   (defalias 'prog-mode 'fundamental-mode))
+
+     ;;; 適用する拡張子
+     (add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
+     (add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
+     (add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
+     (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
+     (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
+     (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+     ;;; インデント数
+     (defun web-mode-hook ()
+       "Hooks for Web mode."
+       (setq web-mode-html-offset   2)
+       (setq web-mode-css-offset    2)
+       (setq web-mode-script-offset 2)
+       (setq web-mode-php-offset    2)
+       (setq web-mode-java-offset   2)
+       (setq web-mode-asp-offset    2)
+     ))
+
+
+
+(add-hook 'web-mode-hook 'web-mode-hook)
+
+(req emmet-mode
+     (add-hook 'sgml-mode-hook 'emmet-mode) ;; マークアップ言語全部で使う
+     (add-hook 'css-mode-hook  'emmet-mode) ;; CSSにも使う
+     (add-hook 'emmet-mode-hook
+               (lambda () (setq emmet-indentation 2))) ;; indent はスペース2個
+     (define-key emmet-mode-keymap (kbd "C-j") nil) ;; C-j は newline のままにしておく
+     (keyboard-translate ?\C-i ?\H-i) ;;C-i と Tabの被りを回避
+     (define-key emmet-mode-keymap (kbd "H-i") 'emmet-expand-line) ;; C-i で展開)
+     )
+
+(req multi-web-mode
+     (setq mweb-default-major-mode 'html-mode)
+     (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                       (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                       (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+     (setq mweb-filename-extensions '("htm" "html" "ctp" "phtml"))
+     (multi-web-global-mode 1))
+
 (provide '50_init-html)
 ;;; 50_init-html ends here
