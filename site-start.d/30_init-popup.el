0e2e8ab6af73302d9985ca2a09d97096a424bcb5
pogin503 <pogin503@gmail.com>
2014-06-22 00:56:03 +0900

    COMMIT-EDITMSGバッファのpopup設定をした

--- site-start.d/30_init-popup.el
+++ site-start.d/30_init-popup.el
@@ -46,7 +46,7 @@
 (push '("*sdic*" :noselect t) popwin:special-display-config)
 (push '("*init log*" :stick t) popwin:special-display-config)
 (push '("\\*magit.*" :stick t :regexp t) popwin:special-display-config)
-(push '("COMMIT-MSG"))
+(push '("COMMIT-EDITMSG" :height 15) popwin:special-display-config)
 (push '("*compilation*" :regexp t) popwin:special-display-config)
 (push '("*ert*" :regexp t) popwin:special-display-config)
 
