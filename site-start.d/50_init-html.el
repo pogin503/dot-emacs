4982a1d54dafe6e4c8c25c85b7f892062a9fd3a5
pogin503 <pogin503@gmail.com>
2014-06-22 00:52:56 +0900

    add-hookを(req)の外に出した

--- site-start.d/50_init-html.el
+++ site-start.d/50_init-html.el
@@ -34,14 +34,14 @@
 (add-hook 'web-mode-hook 'web-mode-hook)
 
 (req emmet-mode
-     (add-hook 'sgml-mode-hook 'emmet-mode) ;; マークアップ言語全部で使う
-     (add-hook 'css-mode-hook  'emmet-mode) ;; CSSにも使う
-     (add-hook 'emmet-mode-hook
-               (lambda () (setq emmet-indentation 2))) ;; indent はスペース2個
      (define-key emmet-mode-keymap (kbd "C-j") nil) ;; C-j は newline のままにしておく
      (keyboard-translate ?\C-i ?\H-i) ;;C-i と Tabの被りを回避
      (define-key emmet-mode-keymap (kbd "H-i") 'emmet-expand-line) ;; C-i で展開)
      )
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; マークアップ言語全部で使う
+(add-hook 'css-mode-hook  'emmet-mode) ;; CSSにも使う
+(add-hook 'emmet-mode-hook
+          (lambda () (setq emmet-indentation 2))) ;; indent はスペース2個
 
 (req multi-web-mode
      (setq mweb-default-major-mode 'html-mode)
