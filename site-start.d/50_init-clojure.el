9b8b70b1f23be8cc8b2468e0327aa5c2f13a4a96
pogin503 <pogin503@gmail.com>
2014-06-25 19:44:36 +0900

    Add header and footer

--- site-start.d/50_init-clojure.el
+++ site-start.d/50_init-clojure.el
@@ -1,4 +1,14 @@
+;;; 50_init-clojure --- 50_init-clojure
+;; This program is free software
+;;; Commentary:
+;;; Code:
+
+(require '00_init-macro)
+
 (when (autoload-if-found 'clojure-mode "clojure-mode" nil t)
   (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
   (lazyload (clojure-mode) "clojure-mode"
             (require 'clojure-mode)))
+
+(provide '50_init-clojure)
+;;; 50_init-clojure ends here
