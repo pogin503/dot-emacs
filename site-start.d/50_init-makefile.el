4f7bd1e850d3ff24ec75808feafa2390c5f57a81
pogin503 <pogin503@gmail.com>
2014-08-04 19:38:37 +0900

    Update header and footer

--- site-start.d/50_init-makefile.el
+++ site-start.d/50_init-makefile.el
@@ -1,4 +1,10 @@
+;;; 50_init-makefile.el --- 50_init-makefile.el
+;; This program is free software
+;;; Commentary:
+;;; Code:
 (add-hook 'makefile-mode-hook
           (lambda ()
             (setq tab-width 8)
             (setq indent-tabs-mode t)))
+(provide '50_init-makefile)
+;;; 50_init-makefile.el ends here
