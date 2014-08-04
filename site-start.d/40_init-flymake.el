27af73698a8b289bb6b0e166f7f003ead84246be
pogin503 <pogin503@gmail.com>
2014-08-04 19:38:01 +0900

    Flaymakeの設定を無効化

--- site-start.d/40_init-flymake.el
+++ site-start.d/40_init-flymake.el
@@ -18,67 +18,67 @@
 ;; (setq temporary-file-directory nil)
 ;; (setq temporary-file-directory nil)
 ;; (load "~/.emacs.d/plugins/flymake/flymake.elc")
-(require 'flymake)
+;; (require 'flymake)
 
-(defun flymake-simple-generic-init (cmd &optional opts)
-  (let* ((temp-file  (flymake-init-create-temp-buffer-copy
-                      'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list cmd (append opts (list local-file)))))
+;; (defun flymake-simple-generic-init (cmd &optional opts)
+;;   (let* ((temp-file  (flymake-init-create-temp-buffer-copy
+;;                       'flymake-create-temp-inplace))
+;;          (local-file (file-relative-name
+;;                       temp-file
+;;                       (file-name-directory buffer-file-name))))
+;;     (list cmd (append opts (list local-file)))))
 
-;; Makefile が無くてもC/C++のチェック
-(defun flymake-simple-make-or-generic-init (cmd &optional opts)
-  (if (file-exists-p "Makefile")
-      (flymake-simple-make-init)
-    (flymake-simple-generic-init cmd opts)))
+;; ;; Makefile が無くてもC/C++のチェック
+;; (defun flymake-simple-make-or-generic-init (cmd &optional opts)
+;;   (if (file-exists-p "Makefile")
+;;       (flymake-simple-make-init)
+;;     (flymake-simple-generic-init cmd opts)))
 
-(defun flymake-c-init ()
-  (flymake-simple-make-or-generic-init
-   "gcc" '("-Wall" "-Wextra" "-pedantic" "-fsyntax-only")))
-
-(defun flymake-cc-init ()
-  (flymake-simple-make-or-generic-init
-   "g++" '("-Wall" "-Wextra" "-pedantic" "-fsyntax-only")))
-
-;; (defun flymake-ruby-init ()
+;; (defun flymake-c-init ()
 ;;   (flymake-simple-make-or-generic-init
-;;    "ruby" '("-c")))
+;;    "gcc" '("-Wall" "-Wextra" "-pedantic" "-fsyntax-only")))
 
-;; (defun flymake-ruby-init ()
+;; (defun flymake-cc-init ()
+;;   (flymake-simple-make-or-generic-init
+;;    "g++" '("-Wall" "-Wextra" "-pedantic" "-fsyntax-only")))
+
+;; ;; (defun flymake-ruby-init ()
+;; ;;   (flymake-simple-make-or-generic-init
+;; ;;    "ruby" '("-c")))
+
+;; ;; (defun flymake-ruby-init ()
+;; ;;   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+;; ;;                        'flymake-create-temp-inplace))
+;; ;;          (local-file  (file-relative-name
+;; ;;                        temp-file
+;; ;;                        (file-name-directory buffer-file-name))))
+;; ;;     (list "ruby" (list "-c" local-file))))
+
+;; (add-hook 'ruby-mode-hook '(lambda () (flymake-mode 1)))
+;; ;; (require 'flymake-ruby)
+;; ;; (add-hook 'ruby-mode-hook 'flymake-ruby-load)
+
+;; ;; flymakeがエラーになっても停止しないようにするための設定
+;; (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+;;   (setq flymake-check-was-interrupted t))
+;; (ad-activate 'flymake-post-syntax-check)
+
+;; ;; (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+;; ;; (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+;; (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+
+;; (push '("\\.[cC]\\'" flymake-c-init) flymake-allowed-file-name-masks)
+;; (push '("\\.\\(?:cc\|cpp\|CC\|CPP\\)\\'" flymake-cc-init) flymake-allowed-file-name-masks)
+
+;; (defun flymake-cc-init ()
 ;;   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
 ;;                        'flymake-create-temp-inplace))
 ;;          (local-file  (file-relative-name
 ;;                        temp-file
 ;;                        (file-name-directory buffer-file-name))))
-;;     (list "ruby" (list "-c" local-file))))
+;;     (list "g++" (list "-Wall" "-Wextra" "-fsyntax-only" local-file))))
 
-(add-hook 'ruby-mode-hook '(lambda () (flymake-mode 1)))
-;; (require 'flymake-ruby)
-;; (add-hook 'ruby-mode-hook 'flymake-ruby-load)
-
-;; flymakeがエラーになっても停止しないようにするための設定
-(defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
-  (setq flymake-check-was-interrupted t))
-(ad-activate 'flymake-post-syntax-check)
-
-;; (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-;; (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
-
-(push '("\\.[cC]\\'" flymake-c-init) flymake-allowed-file-name-masks)
-(push '("\\.\\(?:cc\|cpp\|CC\|CPP\\)\\'" flymake-cc-init) flymake-allowed-file-name-masks)
-
-(defun flymake-cc-init ()
-  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-         (local-file  (file-relative-name
-                       temp-file
-                       (file-name-directory buffer-file-name))))
-    (list "g++" (list "-Wall" "-Wextra" "-fsyntax-only" local-file))))
-
-(push '("\\.cpp$" flymake-cc-init) flymake-allowed-file-name-masks)
+;; (push '("\\.cpp$" flymake-cc-init) flymake-allowed-file-name-masks)
 
 ;; (add-hook 'c++-mode-hook
 ;;           '(lambda () (flymake-mode 1)))
