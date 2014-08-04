43e050b2e8998ba33b91bd7648e7a39033c7a785
pogin503 <pogin503@gmail.com>
2014-08-04 19:48:59 +0900

    Update ruby-setting

--- site-start.d/50_init-ruby-mode.el
+++ site-start.d/50_init-ruby-mode.el
@@ -11,18 +11,18 @@
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 
-(autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
-(add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
+;; (autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
+;; (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
 
 (eval-after-load 'ruby-mode
   (progn
     ;; ruby-block
-    (req ruby-block)
-    (ruby-block-mode t)
+    ;; (req ruby-block)
+    ;; (ruby-block-mode t)
     ;; ミニバッファに表示し, かつ, オーバレイする.
     (setq ruby-block-highlight-toggle t)
-    (defun ruby-mode-hook-ruby-block()
-      (ruby-block-mode t))
+    ;; (defun ruby-mode-hook-ruby-block()
+    ;;   (ruby-block-mode t))
     (req ruby-electric)
     ;; set ruby-mode indent
     (setq ruby-indent-level 2)
@@ -34,22 +34,21 @@
 
 ;; (setq ruby-electric-expand-delimiters-list nil)
 
+;; (add-hook 'ruby-mode-hook 'ruby-mode-hook-ruby-block)
 
-(add-hook 'ruby-mode-hook 'ruby-mode-hook-ruby-block)
-
+(require 'mylib)
 (require 'ruby-end)
-
 (add-to-list 'auto-mode-alist '(".erb$" . rhtml-mode))
 
-(defun html-setting ()
-  (setq c-basic-offset 1
-        tab-width 1))
+;; (defun html-setting ()
+;;   (setq c-basic-offset 1
+;;         tab-width 1))
 
-(add-hook 'rhtml-mode-hook 'html-setting)
-(require 'rhtml-mode)
-(require 'rinari)
-(add-hook 'rhtml-mode-hook
-    (lambda () (rinari-launch)))
+;; (add-hook 'rhtml-mode-hook 'html-setting)
+;; (require 'rhtml-mode)
+;; (require 'rinari)
+;; (add-hook 'rhtml-mode-hook
+;;     (lambda () (rinari-launch)))
 
 (req haml-mode)
 
