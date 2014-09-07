c8becbf252fa9fbdede178d19976e9e815326731
pogin <pogin503@gmail.com>
2014-09-07 21:05:10 +0900

    Update clojure setting

--- site-start.d/50_init-clojure.el
+++ site-start.d/50_init-clojure.el
@@ -10,5 +10,23 @@
   (lazyload (clojure-mode) "clojure-mode"
             (require 'clojure-mode)))
 
+(add-hook 'clojure-mode-hook 'cider-mode)
+
+;; mini bufferに関数の引数を表示させる
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+;; 'C-x b' した時に *nrepl-connection* と *nrepl-server* のbufferを一覧に表示しない
+(setq nrepl-hide-special-buffers t)
+
+;; RELPのbuffer名を 'project名:nREPLのport番号' と表示する
+;; project名は project.clj で defproject した名前
+(setq nrepl-buffer-name-show-port t)
+
+(autoload 'ac-nrepl "ac-nrepl" nil t)
+(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'cider-repl-mode))
+
 (provide '50_init-clojure)
 ;;; 50_init-clojure ends here
