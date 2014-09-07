1b823333acb75518b4b3d8fa3778988d2d83f97d
pogin <pogin503@gmail.com>
2014-09-07 21:21:55 +0900

    Update r-like-example setting

--- site-start.d/30_init-r-like-example.el
+++ site-start.d/30_init-r-like-example.el
@@ -16,15 +16,7 @@
 ;; (add-to-list 'popwin:special-display-config '("*example*" :position right :width 45 :stick t))
 (push '("*example*" :position right :width 45 :stick t) popwin:special-display-config)
 
-;; (pop popwin:special-display-config)
-(global-set-key (kbd "s-9") 'ex-example)
-(global-set-key (kbd "M-9") 'ex-example)
-(global-set-key (kbd "s-0") 'ex-store-key-example)
-(global-set-key (kbd "C-c 0 a") 'ex-add-example)
-(global-set-key (kbd "C-c 0 i") 'ex-insert-current-buffer)
-(global-set-key (kbd "C-c 0 p") 'ex-put-to-example)
-;; (global-set-key (kbd "C-c 9") 'ex-deque-example)
-(global-set-key (kbd "C-c 0 d") 'ex-delete-last-elem)
+(ex-set-keybinding)
 
 (provide '30_init-r-like-example)
 ;;; 30_init-r-like-example ends here
