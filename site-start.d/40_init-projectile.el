b923bab3a790b8fc40efe2b284d66b78f2775219
pogin <pogin503@gmail.com>
2014-09-07 21:18:25 +0900

    Add elisp

---
+++ site-start.d/40_init-projectile.el
@@ -0,0 +1,11 @@
+;;; 40_init-projectile.el --- 40_init-projectile.el
+;; Author: pogin
+;;; Commentary:
+;; This program is free software
+;;; Code:
+
+;;; Projectile
+(projectile-global-mode)
+
+(provide '40_init-projectile)
+;;; 40_init-projectile.el ends here
