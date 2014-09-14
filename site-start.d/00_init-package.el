ef03c3d2ffc387a78073565f4fb171339821b446
pogin <pogin503@gmail.com>
2014-09-15 01:59:03 +0900

    Remove comment

--- site-start.d/00_init-package.el
+++ site-start.d/00_init-package.el
@@ -9,28 +9,13 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
-;;リポジトリにMarmaladeを追加
-;; (add-to-list 'package-archives
-;; 			 '("tromy" . "http://tromey.com/elpa/"))
-;; (add-to-list 'package-archives
-;; 			   '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (add-to-list 'package-archives
-'("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
 
-;; ;;インストールするディレクトリを指定
-;; (setq package-user-dir (concat user-emacs-directory "elpa"))
-
-;;インストールしたパッケージにロードパスを通してロードする
 (package-initialize)
-
-;; install packages by package.el
-;; (mapc
-;;  (lambda (package)
-;;    (when (not (package-installed-p package))
-;;      (package-install package)))
 
 (defconst installing-package-list
   '(
@@ -91,13 +76,5 @@
 
 ;; (require 'mylib)
 ;; (my-install-package installing-package-list)
-
-;; cask setting
-
-;; .cask
-;; cask init
-(add-to-list 'auto-mode-alist '("Cask$" . emacs-lisp-mode))
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
 
 ;;; 00_init-package.el ends here
