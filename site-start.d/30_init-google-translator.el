7d6b34564f7038e86b469a55807222525d6253ff
pogin <pogin503@gmail.com>
2014-09-23 00:30:59 +0900

    text-translator から google-translatorに変更

---
+++ site-start.d/30_init-google-translator.el
@@ -0,0 +1,19 @@
+;;; 30_init-google-translator.el --- 30_init-google-translator.el
+;; This program is free software
+;;; Commentary:
+;;; Code:
+
+;;; 30_init-google-translator.el --- for google-translator conf
+
+;;; Code:
+
+(require 'google-translate)
+(require 'google-translate-default-ui)
+(global-set-key "\C-ct" 'google-translate-at-point)
+(global-set-key "\C-cT" 'google-translate-query-translate)
+
+;; | C-c t | ポイント下の単語を翻訳 |
+;; | C-c T | ミニバッファから読み込む |
+
+(provide '30_init-google-translator)
+;;; 30_init-google-translator.el ends here
