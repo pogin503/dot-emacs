8f599df8cba73816e17385c2e3da417fe99dd9ca
pogin <pogin503@gmail.com>
2014-09-27 02:21:46 +0900

    Add sequential-command conf

---
+++ site-start.d/40_init-sequential-command.el
@@ -0,0 +1,10 @@
+;;; 40_init-sequential-command.el --- 40_init-sequential-command.el
+;;; Commentary:
+;; This program is free software
+;;; Code:
+
+(require 'sequential-command-config)
+(sequential-command-setup-keys)
+
+(provide '40_init-sequential-command)
+;;; 40_init-sequential-command.el ends here
