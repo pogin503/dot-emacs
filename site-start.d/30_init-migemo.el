c809b6dbb14d2365b57b0cfd928d6c96020f044a
pogin <pogin503@gmail.com>
2014-10-02 23:18:11 +0900

    謎のオプションを削除した

--- site-start.d/30_init-migemo.el
+++ site-start.d/30_init-migemo.el
@@ -6,7 +6,14 @@
 (when (executable-find "cmigemo")
   (require 'migemo)
   (setq migemo-command "/usr/local/bin/cmigemo")
-  (setq migemo-options '("-q" "--emacs" "-i" "\g"))
+  ;; -d --dict <dict>       Use a file <dict> for dictionary.
+  ;; -s --subdict <dict>    Sub dictionary files. (MAX 8 times)
+  ;; -q --quiet             Show no message except results.
+  ;; -v --vim               Use vim style regexp.
+  ;; -e --emacs             Use emacs style regexp.
+  ;; -n --nonewline         Don't use newline match.
+  ;; -w --word <word>       Expand a <word> and soon exit.
+  (setq migemo-options '("-q" "--emacs"))
   (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
