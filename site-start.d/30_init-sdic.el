664b37571fe8eec60acdf0c18dacddd431a2c9b6
pogin <pogin503@gmail.com>
2014-10-02 23:36:03 +0900

    Improve setting

--- site-start.d/30_init-sdic.el
+++ site-start.d/30_init-sdic.el
@@ -2,24 +2,29 @@
 ;; This program is free software
 ;;; Commentary:
 ;;; Code:
-(add-to-list 'load-path "~/.emacs.d/plugins/sdic/")
-(add-to-list 'load-path "~/.emacs.d/etc/dict/")
+
+(require 'sdic)
+
+(add-to-list 'load-path (concat user-emacs-directory "plugins/sdic/"))
+(add-to-list 'load-path (concat user-emacs-directory "etc/dict/"))
 
 ;;; sdic-mode 用の設定
-(autoload 'sdic-describe-word
-  "sdic" "英単語の意味を調べる" t nil)
+(autoload 'sdic-describe-word "sdic" "英単語の意味を調べる" t nil)
 (global-set-key "\C-cw" 'sdic-describe-word)
+
 (autoload 'sdic-describe-word-at-point
   "sdic" "カーソルの位置の英単語の意味を調べる" t nil)
 (global-set-key "\C-cW" 'sdic-describe-word-at-point)
 
+;; | C-c w | 英単語の意味を調べる |
+;; | C-c W | カーソルの位置の英単語の意味を調べる |
+
 (setq sdic-eiwa-dictionary-list
-;      '((sdicf-client "/home/ogin/.emacs.d/etc/dict/gene.sdic"))
-      '((sdicf-client "~/.emacs.d/etc/dict/gene.sdic"))
-      ;; 和英検索で使用する辞書
-      sdic-waei-dictionary-list
-;      '((sdicf-client "/home/ogin/.emacs.d/etc/dict/jedict.sdic")))
-      '((sdicf-client "~/.emacs.d/etc/dict/jedict.sdic")))
+      '((sdicf-client (concat user-emacs-directory "etc/dict/gene.sdic"))))
+
+;; 和英検索で使用する辞書
+(setq sdic-waei-dictionary-list
+      '((sdicf-client (concat user-emacs-directory "etc/dict/jedict.sdic"))))
 
 ;;文字エンコード
 ;(setq sdic-default-coding-system 'utf-8)
@@ -27,37 +32,40 @@
 ;; 文字色
 (setq sdic-face-color "pink")
 
-;;./configure --with-emacs=emacs --with-dictdir=~/.emacs.d/etc/dict --with-dicttype=sdic --with-lispdir=~/.emacs.d/plugins/sdic
-;;sudo make install
-;;sudo make install-info
-;;make dict
-;;sudo make install-dict
-
 ;;@see http://hgw09.exblog.jp/12670557/
+;; ポイント下にある単語の意味を辞書で引き、意味を自動でミニバッファに表示
 (req sdic-inline)
 (sdic-inline-mode t) ; sdic-inline モードの起動
-(setq sdic-inline-word-at-point-strict t)
+(setq sdic-inline-word-at-point-strict t) ; ポイント下に単語がある場合のみマッチする
 
-; 辞書ファイルの設定
-(setq sdic-inline-eiwa-dictionary "~/.emacs.d/etc/dict/gene.sdic")
-(setq sdic-inline-waei-dictionary "~/.emacs.d/etc/dict/jedict.sdic")
+;; 辞書ファイルの設定
+(setq sdic-inline-eiwa-dictionary (concat user-emacs-directory "etc/dict/gene.sdic"))
+(setq sdic-inline-waei-dictionary (concat user-emacs-directory "etc/dict/jedict.sdic"))
 
-;sdic tooltip
+;; sdic-inline を有効とするメジャーモードを収めたリスト。
+;; sdic-inline-enable-modes
+;; sdic-inline を有効とするテキストプロパティを収めたリスト。
+;; sdic-inline-enable-faces
+;; 以下に設定された正規表現とファイル名がマッチする場合、sdic-inline を有効とする。
+;; sdic-inline-enable-filename-regex
+;; この変数に指定された関数の実行結果が t を返す場合、sdic-inline を有効とする。
+;; sdic-inline-enable-func
+
+;; sdic tooltip
 (req sdic-inline-pos-tip)
 (setq sdic-inline-display-func 'sdic-inline-pos-tip-show)
-
-(setq transient-mark-mode t)
 
 (defun sdic-inline-pos-tip-show-when-region-selected (entry)
   (cond
    ((and transient-mark-mode mark-active)
     (funcall 'sdic-inline-pos-tip-show entry))
    (t
-; (funcall 'sdic-inline-display-minibuffer entry)
+    ;; (funcall 'sdic-inline-display-minibuffer entry)
     )))
 
 (setq sdic-inline-search-func 'sdic-inline-search-word-with-stem)
 (setq sdic-inline-display-func 'sdic-inline-pos-tip-show-when-region-selected)
 (define-key sdic-inline-map "\C-c\C-p" 'sdic-inline-pos-tip-show)
+
 (provide '30_init-sdic)
 ;;; 30_init-sdic ends here
