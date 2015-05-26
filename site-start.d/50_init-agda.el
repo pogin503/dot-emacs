6e4e4186a8fc4b173dbe67ee3f4c631dca17987b
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-05-26 22:52:20 +0900

    Add the setup procedure of Agda and agda-mode

--- site-start.d/50_init-agda.el
+++ site-start.d/50_init-agda.el
@@ -1,16 +1,23 @@
 ;;; 50_init-agda.el --- 50_init-agda.el
-;; Author: Ryo
-;; Version:
-;; Package-Requires: ()
 ;;; Commentary:
+;;
 ;; This program is free software
+;;
+;; Install Agda
+;;
+;; cabal install agda
+;; cabal install Agda
+;; agda-mode setup
+;;
+;; (load-file (let ((coding-system-for-read 'utf-8))
+;;              (shell-command-to-string "agda-mode locate")))
+;;
+;; agda-mode compile
+;;
 ;;; Code:
 
-;; agda-mode
-(load-file
- (let ((coding-system-for-read 'utf-8))
-   (shell-command-to-string "agda-mode locate")))
-
+(load-file (let ((coding-system-for-read 'utf-8))
+             (shell-command-to-string "agda-mode locate")))
 
 (provide '50_init-agda)
 ;;; 50_init-agda.el ends here
