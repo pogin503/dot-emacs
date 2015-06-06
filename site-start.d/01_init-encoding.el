5673e2409e877db64198521d3324ac70e092d883
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-06-06 21:47:06 +0900

    init-loaderの挙動に合わせて設定を移動
    window-system = darwinだと"cocoa-emacs-" プレフィクスのファイル読み込ん
    でくれないため01_init_encodingに設定を移動した。

--- site-start.d/01_init-encoding.el
+++ site-start.d/01_init-encoding.el
@@ -16,5 +16,20 @@
 ;; (add-to-list â€˜mime-charset-coding-system-alist
 ;;              â€˜(iso-2022-jp . cp50220))
 
+;; encoding
+(require 'ucs-normalize)
+(prefer-coding-system 'utf-8)
+(setq file-name-coding-system 'utf-8-hfs)
+(setq locale-coding-system 'utf-8-hfs)
+
+;; @see https://gist.github.com/sky-y/3264252
+(defun ucs-normalize-NFC-buffer ()
+  "ãƒãƒƒãƒ•ã‚ï¼è³¢ãõ€”‹¿ç‚åŠ«ˆ†é›â‰ª‚’ç›çœ¼—ãéšœ™ã€‚"
+  (interactive)
+  (ucs-normalize-NFC-region (point-min) (point-max))
+  )
+
+(global-set-key (kbd "C-x RET u") 'ucs-normalize-NFC-buffer)
+
 (provide '01_init-encoding)
 ;;; 01_init-encoding ends here
