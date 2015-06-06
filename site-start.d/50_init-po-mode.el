db5dfbecdfe929efa62e974a0a614e90b468e20d
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-06-06 23:45:39 +0900

    Update po-mode setting

--- site-start.d/50_init-po-mode.el
+++ site-start.d/50_init-po-mode.el
@@ -1,4 +1,4 @@
-(add-to-list 'load-path "~/.emacs.d/plugins/gettext")
+(add-to-list 'load-path (concat user-emacs-directory "plugins/gettext"))
 
 (setq auto-mode-alist
 	  (cons '("\\.po\\'\\|\\.po\\." . po-mode) auto-mode-alist))
