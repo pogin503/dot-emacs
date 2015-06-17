74eb5e71438c5662cddaeade136defb866951a61
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-06-18 01:24:22 +0900

    Update el-get basic setup

--- 00_init-el-get.el
+++ 00_init-el-get.el
@@ -6,20 +6,17 @@
 ;; So the idea is that you copy/paste this code into your *scratch* buffer,
 ;; hit C-j, and you have a working developper edition of el-get.
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(add-to-list 'load-path (concat user-emacs-directory "el-get/el-get"))
 
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
-;; レシピ置き場
-(add-to-list 'el-get-recipe-path  "~/.emacs.d/el-get/el-get-user/recipes")
+(add-to-list 'el-get-recipe-path (concat user-emacs-directory "el-get-user/recipes"))
 (el-get 'sync)
-
-(setq el-get-verbose t)
 
 (provide '00_init-el-get)
 ;;; 00_init-el-get ends here
