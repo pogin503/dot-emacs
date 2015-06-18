33074557597b8a663e5a135962df66fedd3e916b
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-06-18 23:29:08 +0900

    Move 00_init-el-get.el from ./ to ./site-start.d/

--- site-start.d/00_init-el-get.el
+++ site-start.d/00_init-el-get.el
@@ -6,7 +6,7 @@
 ;; So the idea is that you copy/paste this code into your *scratch* buffer,
 ;; hit C-j, and you have a working developper edition of el-get.
 
-(add-to-list 'load-path (concat user-emacs-directory "el-get/el-get"))
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
 
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
@@ -15,8 +15,9 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
-(add-to-list 'el-get-recipe-path (concat user-emacs-directory "el-get-user/recipes"))
+(add-to-list 'el-get-recipe-path (locate-user-emacs-file "el-get-user/recipes"))
 (el-get 'sync)
+(el-get-bundle tarao/with-eval-after-load-feature-el)
 
 (provide '00_init-el-get)
 ;;; 00_init-el-get ends here
