583915d0eceb479114ec7b83524e524eb7040a76
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-07-03 23:56:41 +0900

    Update flyspell-mode setting

--- site-start.d/40_init-flyspell.el
+++ site-start.d/40_init-flyspell.el
@@ -2,30 +2,23 @@
 ;; This program is free software
 ;;; Commentary:
 ;;; Code:
+
 ;;@see http://lcw-pon.blogspot.com/2009/12/emacs.html#more
-;; Aspell
-;;; ispellをより高機能なaspellに置き換える
-(setq flyspell-issue-welcome-flag nil)
-;; (setq-default ispell-program-name "aspell")
-;; (setq ispell-grep-command "grep")
-;; ;;; 日本語ファイル中のスペルチェックを可能にする
-;; (eval-after-load "ispell"
-;;   '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
 
-;; ;; Flyspell
-;; ;;; FlySpellの逐次スペルチェックを使用するモードの指定
-;; (defun my-flyspell-mode-enable ()
-;;   (flyspell-mode 1))
-;; (setq flyspell-issue-welcome-flag nil)
+;;; 日本語ファイル中のスペルチェックを可能にする
+(eval-after-load "ispell"
+  '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
 
-;; (mapc
-;;  (lambda (hook)
-;;    (add-hook hook 'my-flyspell-mode-enable))
-;;  '(changelog-mode-hook
-;;    text-mode-hook
-;;    latex-mode-hook)
-;;    ;;; Flyspellの逐次スペルチェックをコメントにのみ使用するモード
-;;    ;;; (コメントかどうかの判断は各モードによる
-;;  )
+(defun my-flyspell-mode-enable ()
+  (flyspell-mode 1))
+
+(mapc
+ (lambda (hook)
+   (add-hook hook 'my-flyspell-mode-enable))
+ '(changelog-mode-hook
+   text-mode-hook
+   latex-mode-hook
+   git-commit-mode-hook))
+
 (provide '40_init-flyspell)
 ;;; 40_init-flyspell ends here
