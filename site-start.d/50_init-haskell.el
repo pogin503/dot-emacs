ed1487551dd4ec6cb827157c7a9dc3a5e4d0b3c9
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-07-05 13:43:38 +0900

    Update haskell-mode setting

--- site-start.d/50_init-haskell.el
+++ site-start.d/50_init-haskell.el
@@ -3,52 +3,30 @@
 ;;; Commentary:
 ;;; Code:
 
-;; #!/usr/bin/env runghc 用
-(add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))
-;; #!/usr/bin/env runhaskell 用
-(add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode))
+(use-package haskell-mode
+  :commands (haskell-mode literate-haskell-mode haskell-cabal-mode)
+  :interpreter (("runghc" . haskell-mode)      ; #!/usr/bin/env runghc 用
+				("runhaskell" . haskell-mode)) ; #!/usr/bin/env runhaskell 用
+  :mode (("\\.l?hs$" . haskell-mode)
+		 ("\\.cabal$" . haskell-cabal-mode))
+  :config
+  (use-package ghc
+	:config
+	;; ghc-mod setting
+    (autoload 'ghc-init "ghc" nil t)
+    (autoload 'ghc-debug "ghc" nil t)
+    (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+    ;; エコーエリアに関数の型を表示するモードをオンにする
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+
+    ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent))
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+    ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+    ))
 
 (add-to-list 'auto-mode-alist '("\\.hamlet$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.lucius$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.julius$" . js2-mode))
-
-(require 'mylib)
-
-(require '00_init-macro)
-(lazyload (haskell-mode literate-haskell-mode haskell-cabal-mode) "haskell-mode"
-          (require 'haskell-mode)
-          (require 'haskell-cabal)
-          (require 'inf-haskell)
-
-          (defun my-ac-haskell-mode ()
-            (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod)))
-          )
-
-;; エコーエリアに関数の型を表示するモードをオンにする
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent))
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-
-(use-package haskell-mode
-  :commands (haskell-mode literate-haskell-mode haskell-cabal-mode)
-  :interpreter (("runghc" . haskell-mode)
-				("runhaskell" . haskell-mode))
-  :mode (("\\.l?hs$" . haskell-mode)
-		 ("\\.cabal$" . haskell-cabal-mode)
-		 )
-  :config
-  (use-package ghc
-	:config
-	(progn
-	  ;; ghc-mod setting
-      (autoload 'ghc-init "ghc" nil t)
-      (autoload 'ghc-debug "ghc" nil t)
-      (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-	  ))
-  )
-
 
 ;; エコーエリアに関数の型を表示するモードをオンにする
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
@@ -96,12 +74,6 @@
              '(haskell-left-arrows
                (regexp . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+")
                (modes quote (haskell-mode literate-haskell-mode))))
-
-;; @see http://karky7.blogspot.jp/2012/12/gentooemacshaskell.html
-;; (defadvice inferior-haskell-load-file (after change-focus-after-load)
-;;   "Change focus to GHCi window after \\<haskell-mode-map>\\[inferior-haskell-load-file] command."
-;;   (other-window 1))
-;; (ad-activate 'inferior-haskell-load-file)
 
 (provide '50_init-haskell)
 ;;; 50_init-haskell ends here
