be837c2717b7a5079cba94b9d89bc8ef80c78213
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-07-24 23:41:53 +0900

    Fix haskell-mode setting

--- site-start.d/50_init-haskell.el
+++ site-start.d/50_init-haskell.el
@@ -17,7 +17,7 @@
 
 (use-package haskell-mode
   :commands (haskell-mode literate-haskell-mode haskell-cabal-mode)
-  :interpreter (("runghc" . haskell-mode)      ; #!/usr/bin/env runghc 用
+  :interpreter (("runghc" . haskell-mode) ; #!/usr/bin/env runghc 用
 				("runhaskell" . haskell-mode)) ; #!/usr/bin/env runhaskell 用
   :mode (("\\.l?hs$" . haskell-mode)
 		 ("\\.cabal$" . haskell-cabal-mode))
@@ -27,30 +27,28 @@
 	;; ghc-mod setting
     (autoload 'ghc-init "ghc" nil t)
     (autoload 'ghc-debug "ghc" nil t)
-    (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-    ;; エコーエリアに関数の型を表示するモードをオンにする
-    (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+    (add-hook 'haskell-mode-hook (lambda () (ghc-init))))
 
-    ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent))
-    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-    ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-    ;; エコーエリアに関数の型を表示するモードをオンにする
-    (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+  ;; エコーエリアに関数の型を表示するモードをオンにする
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 
-    (add-hook 'haskell-mode-hook 'my-haskell-mode-keybinds)
-    (add-hook 'haskell-mode-hook 'my-haskell-mode-conf)
-    (add-hook 'haskell-mode-hook 'my-key-combo-haskell-conf)))
+  ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent))
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+  ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+
+  (defun my-haskell-mode-conf ()
+    "Set haskell-mode config."
+    (my-ac-haskell-mode)
+    (interactive-haskell-mode))
+
+  (add-hook 'haskell-mode-hook 'my-haskell-mode-conf)
+  (require 'mylib-keycombo)
+  (add-hook 'haskell-mode-hook 'my-key-combo-haskell-conf))
 
 (add-to-list 'auto-mode-alist '("\\.hamlet$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.lucius$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.julius$" . js2-mode))
 
-
-(defun my-haskell-mode-conf ()
-  "Set haskell-mode config."
-  (ghc-init)
-  (my-ac-haskell-mode)
-  (interactive-haskell-mode))
 
 ;; Haskell align setting
 (add-to-list 'align-rules-list
