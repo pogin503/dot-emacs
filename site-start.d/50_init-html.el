e59b9b1110e3db56ef9c2482e18329102137473b
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-07-28 00:40:57 +0900

    Update

--- site-start.d/50_init-html.el
+++ site-start.d/50_init-html.el
@@ -108,5 +108,16 @@
 ;; @memo
 ;; backtab : Shift + TAB
 
+(defun my-code-golf-for-js ()
+   (interactive)
+   (mapc (lambda (x)
+           (replace-regexp (concat " *\\(" x "\\) *") "\\1" nil (point-min) (point-max)))
+           '("-" "=" "\\+" "\\*" "/" "<" ">" "%"))
+   (replace-regexp "[ ]*\\([[:digit:]]+\\)[ ]*" "\\1" nil (point-min) (point-max))
+   (replace-regexp "for " "for" nil (point-min) (point-max))
+   (replace-regexp "var " "" nil (point-min) (point-max))
+   (replace-regexp "{ " "{" nil (point-min) (point-max))
+   (replace-regexp "^[[:space:]]* " "for" nil (point-min) (point-max)))
+
 (provide '50_init-html)
 ;;; 50_init-html ends here
