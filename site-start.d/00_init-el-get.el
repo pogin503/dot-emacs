79f334bc7839bcde97fc902855f679b02d1bfbe6
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-11-01 17:49:43 +0000

    Remove dmacro

--- site-start.d/00_init-el-get.el
+++ site-start.d/00_init-el-get.el
@@ -18,7 +18,6 @@
 (add-to-list 'el-get-recipe-path (locate-user-emacs-file "el-get-user/recipes"))
 (el-get-bundle tarao/with-eval-after-load-feature-el)
 (el-get-bundle syohex/emacs-jazzradio)
-(el-get-bundle dmacro)
 (el-get 'sync)
 
 (provide '00_init-el-get)
