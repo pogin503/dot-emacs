f160191a5f719f81f5b47784c28fefab0c47e1a4
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-12-16 23:55:21 +0900

    Update template.hs

--- etc/autoinsert/template.hs
+++ etc/autoinsert/template.hs
@@ -1,9 +1,11 @@
 import Control.Applicative
+import Data.List
+import Control.Monad
+
+toInt :: String -> Int
+toInt = read
 
 main :: IO ()
 main = do
   let str = "123"
   print str
-  where
-    toInt :: String -> Int
-    toInt = read
