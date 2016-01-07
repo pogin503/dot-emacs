f41e57d42726b38e418394d1da964d4dcdb747f8
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2016-01-07 23:16:00 +0900

    Update auto-complete setting

--- site-start.d/40_init-auto-complete.el
+++ site-start.d/40_init-auto-complete.el
@@ -5,37 +5,34 @@
 
 (use-package auto-complete
   :config
-  (progn
-    (require 'auto-complete-config)
+  (require 'auto-complete)
+  (require 'auto-complete-config)
 
-    (setq ac-comphist-file (concat user-emacs-directory  "etc/ac-comphist.dat"))
-    (ac-config-default)
-    ;; (global-auto-complete-mode t)
-    (setq-default ac-use-comphist t)
-    (setq-default ac-auto-start 4
-                  ac-auto-show-menu 1.0)
-    (define-key ac-mode-map (kbd "M-i") 'auto-complete)
-    (define-key ac-mode-map (kbd "H-i") 'auto-complete)
-    (define-key ac-completing-map "\t" 'ac-complete)
+  (setq ac-comphist-file (concat user-emacs-directory  "etc/ac-comphist.dat"))
+  (ac-config-default)
+  ;; (global-auto-complete-mode t)
+  (setq-default ac-use-comphist t)
+  (setq-default ac-auto-start 4
+                ac-auto-show-menu 1.0)
+  (define-key ac-mode-map (kbd "M-i") 'auto-complete)
+  (define-key ac-mode-map (kbd "H-i") 'auto-complete)
+  (define-key ac-completing-map "\t" 'ac-complete)
 
-    ;; Enterで補完をしないようにする
-    (define-key ac-completing-map "\r" nil)
+  ;; Enterで補完をしないようにする
+  (define-key ac-completing-map "\r" nil)
 
-    (dolist (hook (list
-                   'html-mode-hook
-                   'sgml-mode-hook
-                   'nxml-mode-hook
-                   ))
-      (add-hook hook 'auto-complete-mode))
+  (dolist (hook (list
+                 'html-mode-hook
+                 'sgml-mode-hook
+                 'nxml-mode-hook
+                 ))
+    (add-hook hook 'auto-complete-mode))
 
-    (add-to-list 'ac-modes 'haskell-mode)
-
-    ;; coq
-    (defun my-ac-coq-mode ()
-      "Set coq ac-source."
-      (setq ac-sources '(ac-source-words-in-same-mode-buffers
-                         ac-source-dictionary)))
-    ))
+  ;; coq
+  (defun my-ac-coq-mode ()
+    "Set coq ac-source."
+    (setq ac-sources '(ac-source-words-in-same-mode-buffers
+                       ac-source-dictionary))))
 
 (provide '40_init-auto-complete)
 ;;; 40_init-auto-complete ends here
