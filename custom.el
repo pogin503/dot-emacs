69a93b3650914899f1ad090ec28ead6da25e7429
pogin <pogin503@gmail.com>
2016-02-15 23:45:11 +0900

    Remove face setting

--- custom.el
+++ custom.el
@@ -3,11 +3,4 @@
 ;; This program is free software
 ;;; Code:
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(eshell-prompt ((t (:foreground "maroon2" :bold nil)))))
-
 ;;; custom.el ends here
