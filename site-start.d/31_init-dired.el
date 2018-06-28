66d1a4f068be7ce199dcd8426fd12f1fd5b06e58
pogin503 <pogin503@gmail.com>
2018-06-29 00:23:40 +0900

    feat: Add dired-mouse-find-file

--- site-start.d/31_init-dired.el
+++ site-start.d/31_init-dired.el
@@ -33,6 +33,30 @@
 (setq wdired-allow-to-change-permissions t)
 (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
 
+(define-key dired-mode-map [mouse-2] 'dired-mouse-find-file)
+
+;; https://emacs.stackexchange.com/questions/17566/open-file-from-dired-in-the-same-window-using-the-mouse
+(defun dired-mouse-find-file (event)
+  "In Dired, visit the file or directory name you click on."
+  (interactive "e")
+  (let (window pos file)
+    (save-excursion
+      (setq window (posn-window (event-end event))
+            pos (posn-point (event-end event)))
+      (if (not (windowp window))
+          (error "No file chosen"))
+      (set-buffer (window-buffer window))
+      (goto-char pos)
+      (setq file (dired-get-file-for-visit)))
+    (if (file-directory-p file)
+        (or (and (cdr dired-subdir-alist)
+                 (dired-goto-subdir file))
+            (progn
+              (select-window window)
+              (dired-other-window file)))
+      (select-window window)
+      (find-file (file-name-sans-versions file t)))))
+
 ;; wdired keybinds memo
 ;; | e | wdiredモードになる |
 ;; | C-c C-c | 編集を完了 |
