97dfb43fbd0186f9d6a487b2440d4cefd4b7a59e
pogin503 <pogin503@gmail.com>
2018-10-27 16:52:24 +0900

    feat: Add treemacs setting

---
+++ site-start.d/30_init-treemacs.el
@@ -0,0 +1,17 @@
+;;; 30_init-treemacs.el --- 30_init-treemacs.el -*- lexical-binding: t; coding: utf-8 -*-
+;;; Commentary:
+;; This program is free software
+;;; Code:
+
+(define-key treemacs-mode-map (kbd "+") 'my-make-directory)
+
+(require 'f)
+(defun my-make-directory (dirname)
+  (interactive (list (read-string (format "directory name is (pwd: %s) ? " default-directory))))
+  (if (not (f-exists? (f-join default-directory dirname)))
+      (make-directory (f-join default-directory dirname))
+    (message (format "cannot make directory : %s" dirname)))
+  )
+
+(provide '30_init-treemacs)
+;;; 30_init-treemacs.el ends here
