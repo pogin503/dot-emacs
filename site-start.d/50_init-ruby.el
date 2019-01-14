cb09b0714c5bb4a1bf428e63960ae96e8cdc46a1
pogin503 <pogin503@gmail.com>
2019-01-14 14:46:26 +0900

    feat: Update ruby config

--- site-start.d/50_init-ruby.el
+++ site-start.d/50_init-ruby.el
@@ -15,6 +15,21 @@
    '(inf-ruby-eval-binding "Pry.toplevel_binding")
    '(ruby-insert-encoding-magic-comment nil)))
 
+(defun my-ruby-mode-conf ()
+  (setq company-auto-expand t)
+  (setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
+  (setq company-idle-delay 0) ; 遅延なしにすぐ表示
+  (setq company-minimum-prefix-length 1) ; 何文字打つと補完動作を行うか設定
+  (setq company-selection-wrap-around t) ; 候補の最後の次は先頭に戻る
+  (setq completion-ignore-case t)
+  (setq company-dabbrev-downcase nil)
+  (my-ruby-mode-keybinds)
+  )
+
+(use-package company
+  :config
+  (add-hook 'ruby-mode-hook #'my-ruby-mode-conf)
+  )
 (use-package ruby-end
   :config
   (add-to-list 'auto-mode-alist '(".erb$" . rhtml-mode)))
