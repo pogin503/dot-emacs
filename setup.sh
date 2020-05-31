d060f9f8e4bff43290a17d520f98f45e88e5c098
pogin503 <pogin503@gmail.com>
2020-05-31 15:45:46 +0900

    feat: update ./setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
-cd ~/.emacs.d/
-export PATH="$HOME/.cask/bin:$PATH"
-cask install
-git submodule update --init
+DOT_DIR="$HOME"/workspace/dot-emacs
+ln -sf "$DOT_DIR"/.spacemacs ~/.spacemacs
